4422046a2384401792d20a6fca94f537142c80cb
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-10-29 18:28:42 +0100

    rename state varible in burger menu

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -6,19 +6,17 @@ import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
 const BurgerMenu = () => {
-    const [ hidden, setHidden ] = useState(false);
+    const [ open, setOpen ] = useState(false);
   return (
     <>
         <button 
-            aria-expanded={hidden} 
-            aria-controls="main-menu" 
             className={styles.burgerMenu}
-            onClick={() => setHidden((open) => open = !open)}>
+            onClick={() => setOpen((open) => open = !open)}>
             <FontAwesomeIcon id='burger-icon' icon={faBars} size="1x" />
         </button>
         <div>
-        {hidden && 
-            <nav onClick={() => setHidden((open) => open = !open)} id="main-menu" className={styles.links} aria-hidden={!hidden}>
+        {open && 
+            <nav onClick={() => setOpen((open) => open = !open)} id="main-menu" className={styles.links} aria-hidden={!open}>
                 <HashLink smooth to="/#features">Features</HashLink>
                 <Link to="/contact">Contact</Link>
                 <Link to="/"  id="login-mobile">Sign in / up</Link>
