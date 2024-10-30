1d8ba182cde02808d775d0eeefe4e52d8658c097
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-10-30 13:29:55 +0100

    make the map change dependent on theme

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,10 @@
 import mapboxgl from "mapbox-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useEffect, useRef } from "react"
+import useTheme from "../hooks/useTheme";
 
 const Map = ({ width, height }: {width: string, height: string} ) => {
+  const { theme } = useTheme()
     const mapRef = useRef<mapboxgl.Map | null>(null)
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
@@ -10,21 +12,22 @@ const Map = ({ width, height }: {width: string, height: string} ) => {
         mapboxgl.accessToken = 'pk.eyJ1IjoiZW1hbnVlbGd1c3RhZnpvbiIsImEiOiJja294YndvNm8wZXBmMnBtcGFkZ3I5N3V6In0.t_4FKgw6kJSPGD4LleGWqQ'
         mapRef.current = new mapboxgl.Map({
           container: mapContainerRef.current as HTMLElement,
-          center: [11.9902, 57.6941],
-          zoom: 10.12
+          style: `${theme === 'light' ? "mapbox://styles/mapbox/streets-v11" : "mapbox://styles/mapbox/navigation-night-v1"}`,
+          center: [-76.9902, 38.8341],
+          zoom: 12.12
         });
         new mapboxgl.Marker({ color: '#6366F1',})
-            .setLngLat([11.9842, 57.7141])
+            .setLngLat([-76.9902, 38.8341])
             .addTo(mapRef.current);
 
         new mapboxgl.Marker({ color: '#6366F1',})
-            .setLngLat([11.9642, 57.6841])
+            .setLngLat([-76.9802, 38.8441])
             .addTo(mapRef.current);
     
         return () => {
           if (mapRef.current) mapRef.current.remove()
         }
-      }, [])
+      }, [theme])
 
   return (
     <>
